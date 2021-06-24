70be221f65906106f60a6e361d85bc7a6981f537
note <note.wachirawit@maqe.com>
2021-06-24 23:44:52 +0700

    commit

--- cypress/integration/examples/covertCurrencyFormatter.spec.js
+++ cypress/integration/examples/covertCurrencyFormatter.spec.js
@@ -2,12 +2,11 @@
 
 describe('Test', () => {
 
-		it('Test convert currencyFormatter', () => {
-            const thb = "฿20,000";
-            cy.currencyFormatter(2000000).then(totalPrice =>{
-            assert.equal(totalPrice, thb);
-           
-		});
-		});
+	it('Test convert currencyFormatter', () => {
+      const thb = "฿20,000";
+      cy.currencyFormatter(2000000).then(totalPrice =>{
+      assert.equal(totalPrice, thb);
+	});
+	});
 
 });
