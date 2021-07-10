70cbc3eecd70b047d26dfb62e7959881351c2bc6
note <note.wachirawit@maqe.com>
2021-07-10 10:41:17 +0700

    commit

---
+++ cypress/integration/testApi.spec.js
@@ -0,0 +1,24 @@
+describe('User signup', () => {
+	it('Should log the username from .env  correctly', () => {
+		const test ={
+			"email":"wachirawit.th1@ku.th",
+            "password":"Maqe+",
+            "confirmedPassword":"Maqe4+",
+            "isConsent":"false",
+            "versionId": 1
+		};
+		cy.request({
+			method: 'POST',
+			url: 'https://api.xcp.dev.maqehq.com/api/v1/customers',
+			failOnStatusCode: false,
+			body:test
+            ,
+
+		}).then((resp) => {
+			
+			expect(resp.status).to.eq(200);
+			expect(resp.body.data.isConsent).to.eq('must be true');
+		});
+	});
+
+});
