e9b0dd54fe83cdcd49383e0e7ad19f6eb176a2a9
note wachirawit <wachirawit.th@ku.th>
2021-08-29 13:51:58 +0700

    remove cy.log

--- cypress/integration/pocMailosaur.test.js
+++ cypress/integration/pocMailosaur.test.js
@@ -1,5 +1,5 @@
 describe('Password reset success', () => {
-    const serverId = 'ae3q7nha'; // Replace SERVER_ID with an actual Mailosaur Server ID
+    const serverId = 'ae3q7nha';
     const testEmail = 'note.wachirawit+14@ae3q7nha.mailosaur.net';
     let link ='';
 
@@ -11,12 +11,11 @@ describe('Password reset success', () => {
     });
 
     it('Gets a Password Reset email', () => {
-    cy.wait(5000);
 
+    cy.wait(5000);
     cy.mailosaurListMessages(serverId, { receivedAfter: new Date(2020, 1, 1) }).then((result) => {
     let linkResetpassword = result.items[0].summary;
     link = linkResetpassword;
-    cy.log(link);    
 
 });
     });
