16923245f419bdef36d9cb305dcc86017dc1c1b9
Note <wachirawit.th@ku.th>
2021-12-15 23:29:19 +0700

    edit testcase login

--- cypress/integration/pocGraphql.spec.js
+++ cypress/integration/pocGraphql.spec.js
@@ -1,33 +1,33 @@
-
 ///#https://lucasconstantino.github.io/graphiql-online/
+import { userVipLevel3 } from "../userInformation/userData";
+import { userSignIn } from "../schema/schema";
 let token;
-const urlZ =  Cypress.env("api_url");
+const urlZ = Cypress.env("api_url");
 it("test api", () => {
   const name = "wachirawit.th@ku.th";
   cy.log(Cypress.env("test"));
   assert.equal(name, Cypress.env("username"));
 
-    const query = `{
+  const query = `{
       countries {
         name
         emojiU
         currency
       }
   }`;
-    cy.request({
-      method: "POST",
-      url: "https://countries.trevorblades.com/",
-      body: { query },
-      failOnStatusCode: false,
-    }).then((res) => {
-  
-      cy.log(res.body.data.countries[1].name);
-      expect(res.status).to.eq(200);
-    });
+  cy.request({
+    method: "POST",
+    url: "https://countries.trevorblades.com/",
+    body: { query },
+    failOnStatusCode: false,
+  }).then((res) => {
+    cy.log(res.body.data.countries[1].name);
+    expect(res.status).to.eq(200);
   });
-  
-  it("test graphql query", () => {
-    const query = `{
+});
+
+it("test graphql query", () => {
+  const query = `{
       countries {
         languages {
           name
@@ -35,74 +35,44 @@ it("test api", () => {
         }
       }
   }`;
-    cy.request({
-      method: "POST",
-      url: "https://countries.trevorblades.com/",
-      body: { query },
-      failOnStatusCode: false,
-    }).then((res) => {
-  
-      cy.log(res.body.data.countries[1].languages[0].native);
-      cy.log(res.body.data.countries[1].languages[0].name);
-      expect(res.status).to.eq(200);
-    });
+  cy.request({
+    method: "POST",
+    url: "https://countries.trevorblades.com/",
+    body: { query },
+    failOnStatusCode: false,
+  }).then((res) => {
+    cy.log(res.body.data.countries[1].languages[0].native);
+    cy.log(res.body.data.countries[1].languages[0].name);
+    expect(res.status).to.eq(200);
   });
+});
 
-  it("test graphql query", () => {
-    const mutation = `mutation {
-      signIn(input:{email:"stg.zm.trader.23+zipstarter+th1@gmail.com"
-        ,password: "Abc12345!"})
-      {
-       __typename
-        ... on PreMFA {
-          type
-      
-        }
-        ... on Session{
-          token
-          user {
-            features
-            invitationCode
-            mobileNumber
-            personalInfo
-            referralCode
-            suitabilitySurvey
-            userId
-          }
-        }
-      }
-    }`;
-    cy.request({
-      method: "POST",
-      url: urlZ,
-      body: { query: mutation },
-      failOnStatusCode: false,
-    }).then((res) => {
-      token = res.body.data.signIn.token;
-    });
+it("login user zipmex", () => {
+  cy.apiLogin(userSignIn, userVipLevel3.username, userVipLevel3.password).then(
+    (tokenUser) => {
+      token = tokenUser;
+    }
+  );
+});
 
-  });
-  
-  it("User get bank account success", () => {
-    const query = `{
+it("User get bank account success", () => {
+  const query = `{
       bankAccounts {
       accountName
       bankCode
       isVerified
       }
     }`;
-    cy.request({
-      method: "POST",
-      url: urlZ,
-      headers: {
-        Authorization: "Bearer " + token
+  cy.request({
+    method: "POST",
+    url: urlZ,
+    headers: {
+      Authorization: "Bearer " + token,
     },
-      body: { query: query },
-      
-      failOnStatusCode: false,
-    }).then((res) => {
-      cy.log(res.body.data.bankAccounts );
-      
-    });
+    body: { query: query },
+
+    failOnStatusCode: false,
+  }).then((res) => {
+    cy.log(res.body.data.bankAccounts);
   });
-  
+});
--- cypress/support/commands/api.js
+++ cypress/support/commands/api.js
@@ -1,16 +1,16 @@
-Cypress.Commands.add('apiMutation', (mutations) => {
+Cypress.Commands.add("apiMutation", (mutations) => {
   cy.request({
-    method: 'POST',
-    url: Cypress.env('api_url'),
-    body: { query: 'mutation' + mutations },
+    method: "POST",
+    url: Cypress.env("api_url"),
+    body: { query: "mutation" + mutations },
   });
 });
-Cypress.Commands.add('apiChangeLanguage', (query, token, lang) => {
+Cypress.Commands.add("apiChangeLanguage", (query, token, lang) => {
   cy.request({
-    method: 'POST',
-    url: Cypress.env('api_url'),
+    method: "POST",
+    url: Cypress.env("api_url"),
     headers: {
-      Authorization: 'bearer ' + token,
+      Authorization: "bearer " + token,
     },
     body: {
       query: query,
@@ -19,72 +19,78 @@ Cypress.Commands.add('apiChangeLanguage', (query, token, lang) => {
     },
   });
 });
-Cypress.Commands.add('generateGuid',() =>{
-  let gUid =([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-    return gUid;
+Cypress.Commands.add("generateGuid", () => {
+  let gUid = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+  return gUid;
 });
-Cypress.Commands.add('createTransac', (userId,value,amount,instrument,side) => {
+Cypress.Commands.add(
+  "createTransac",
+  (userId, value, amount, instrument, side) => {
+    cy.generateGuid().then((gUid) => {
+      cy.request({
+        failOnStatusCode: false,
+        method: "POST",
+        url: Cypress.env("otc_url") + "/api/transact",
+        body: {
+          quote_id: gUid,
+          instrument_id: instrument,
+          side: side,
+          amount: amount,
+          value: value,
+          from_service_id: "main_wallet",
+          from_account_id: userId,
+          to_service_id: "card_service",
+          to_account_id: "system",
+          metadata: '{"reference": "ref_001"}',
+        },
+      });
+    });
+  }
+);
+Cypress.Commands.add("topUpUserWallet", (userId, instrument, amount) => {
   cy.generateGuid().then((gUid) => {
-  cy.request({
-    failOnStatusCode: false,
-    method: 'POST',
-    url: Cypress.env('otc_url')+'/api/transact',
-    body: {
-    "quote_id": gUid,
-    "instrument_id": instrument,
-    "side": side,
-    "amount":amount,
-    "value": value,
-    "from_service_id": 'main_wallet',
-    "from_account_id": userId,
-    "to_service_id": 'card_service',
-    "to_account_id": 'system',
-    "metadata": "{\"reference\": \"ref_001\"}"
-    }
+    cy.request({
+      method: "POST",
+      url: Cypress.env("asset_manager_url") + "/api/deposit",
+      body: {
+        service_id: "main_wallet",
+        account_id: userId,
+        product_id: instrument,
+        amount: amount,
+        source: "test",
+        source_ref: "test",
+        ref_caller: "test",
+        ref_action: "deposit",
+        ref_id: gUid,
+        idempotent_key: gUid,
+      },
     });
   });
 });
-Cypress.Commands.add('topUpUserWallet', (userId,instrument,amount) => {
-  cy.generateGuid().then((gUid) => {
-  cy.request({
-    method: 'POST',
-    url: Cypress.env('asset_manager_url')+'/api/deposit',
-    body: {
-      "service_id": "main_wallet",
-      "account_id": userId,
-      "product_id": instrument,
-      "amount": amount,
-      "source": "test",
-      "source_ref": "test",
-      "ref_caller": "test",
-      "ref_action": "deposit",
-      "ref_id": gUid,
-      "idempotent_key": gUid
-    }
-  });
-});
-});
 
-Cypress.Commands.add('apiLogin', (query, user, pass) => {
+Cypress.Commands.add("apiLogin", (query, username, password) => {
   cy.request({
-    method: 'POST',
-    url: Cypress.env('api_url'),
+    method: "POST",
+    url: Cypress.env("api_url"),
     body: {
       query: query,
       variables: {
         signInData: {
-          email: user,
-          password: pass,
+          email: username,
+          password: password,
         },
       },
     },
     failOnStatusCode: false,
   }).then((res) => {
     let token = res.body.data.signIn.token;
-    window.localStorage.setItem('zmToken', res.body.data.signIn.token);
-    window.localStorage.setItem('token', res.body.data.signIn.aptoken);
+    window.localStorage.setItem("zmToken", res.body.data.signIn.token);
+    window.localStorage.setItem("token", res.body.data.signIn.aptoken);
     return token;
   });
 });
